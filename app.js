6fdb0ebd02944b20bd98a22856e8c400c4f80de5
kapsab <bamusikaps@yahoo.com>
2026-07-24 21:16:36 +0200

    Fix: Resolved backend req reference crash and active map rendering hooks in loadPoints

--- app.js
+++ app.js
@@ -31,10 +31,10 @@ app.use(session({
     resave: false,	// this prevents constant session overwrites
     saveUninitialized: false,
     cookie: {
-    	secure: isProduction && !req.headers.host.includes('localhost'),	// must be false for http://localhost
+    	secure: isProduction,	// must be false for http://localhost
     	maxAge: 1000 * 60 * 60 * 24,
     	httpOnly: true,
-    	sameSite: isProduction ? 'none' : 'lax'
+    	sameSite: 'lax'
     } // 24 hours session
 }));
 
